b6a245fd20301416c416e92111097e494d0501de
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-05-01 14:57:40 +0430

    importing react icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import {BsInstagram} from 'react-icons/bs'
+import {BsInstagram , BsBugFill} from 'react-icons/bs'
+import {ImTelegram} from 'react-icons/im'
+
 function App() {
-  return (
-    <div style={{padding : '100px'}}>
-      <BsInstagram />
-    </div>
-  );
+    return (
+        <div style={{padding : '100px'}}>
+            <BsInstagram size='50px' />
+            <ImTelegram size='50px'/>
+            <BsBugFill size='50px' color='#00ff00' />
+        </div>
+    );
 }
 
 export default App;
