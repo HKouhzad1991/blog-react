0963bdad2ac0448e3fcb445a80d651c110c16a40
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-05-02 22:38:56 +0430

    routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import {BsInstagram , BsBugFill} from 'react-icons/bs'
-import {ImTelegram} from 'react-icons/im'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import AddArticle from "./pages/addArticle/AddArticle";
+import Article from "./pages/article/Article";
+import EditArticle from "./pages/editArticle/EditArticle";
 
 function App() {
-    return (
-        <div style={{padding : '100px'}}>
-            <BsInstagram size='50px' />
-            <ImTelegram size='50px'/>
-            <BsBugFill size='50px' color='red' />
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/add-article" element={<AddArticle />} />
+        <Route path="/article/:articleId" element={<Article />} />
+        <Route path="/edit-article/:articleId" element={<EditArticle />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
