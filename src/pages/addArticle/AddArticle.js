d71a536f2127299af0eb7d1736cf41bd2c6626c3
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-09 16:08:38 +0430

    article fixing

--- src/pages/addArticle/AddArticle.js
+++ src/pages/addArticle/AddArticle.js
@@ -9,10 +9,31 @@ function AddArticle() {
       <MyNavbar />
       <div className="formContainer">
         <Form>
-          <Form.Group className="mb-3" >
+          <Form.Group className="mb-3">
             <Form.Label>عنوان مقاله</Form.Label>
             <Form.Control type="text" placeholder="عنوان مقاله را وارد کنید" />
-          
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>توضیح کوتاه </Form.Label>
+            <Form.Control type="text" placeholder="توضیح کوتاه را وارد کنید" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>نویسنده مقاله</Form.Label>
+            <Form.Control type="text" placeholder="نام نویسنده مقاله را وارد کنید" />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>موضوع مقاله</Form.Label>
+            <Form.Control type="text" placeholder="موضوع مقاله را وارد کنید" />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>عکس مقاله</Form.Label>
+            <Form.Control type="text" placeholder="عکس مقاله را وارد کنید" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>مدت زمان خواندن مقاله</Form.Label>
+            <Form.Control type="number" placeholder="" />
           </Form.Group>
 
           <Button variant="primary" type="button">
