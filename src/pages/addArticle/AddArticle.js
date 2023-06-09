8d4d3dbf3a4f0f8a24a22205bc3eb8629f3f7b50
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-09 16:50:51 +0430

    using axios in article

--- src/pages/addArticle/AddArticle.js
+++ src/pages/addArticle/AddArticle.js
@@ -2,8 +2,20 @@ import MyNavbar from "../../components/navbar/MyNavbar";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "./AddArticle.css";
+import axios from "axios";
 
 function AddArticle() {
+  const addArticleHandler = () => {
+    axios.post("http://localhost:5000/articles", {
+      title: "عنوان مقاله دهم",
+      desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک",
+      writter: "میلاد",
+      category: "وب",
+      image: "https://dl.next1code.ir/images/react/article8.webp",
+      readingTime: 55,
+    });
+  };
+
   return (
     <>
       <MyNavbar />
@@ -13,13 +25,21 @@ function AddArticle() {
             <Form.Label>عنوان مقاله</Form.Label>
             <Form.Control type="text" placeholder="عنوان مقاله را وارد کنید" />
           </Form.Group>
+
           <Form.Group className="mb-3">
-            <Form.Label>توضیح کوتاه </Form.Label>
-            <Form.Control type="text" placeholder="توضیح کوتاه را وارد کنید" />
+            <Form.Label>توضیح کوتاه</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="یه توضیح کوتاه در مورد مقاله وارد کنید"
+            />
           </Form.Group>
+
           <Form.Group className="mb-3">
             <Form.Label>نویسنده مقاله</Form.Label>
-            <Form.Control type="text" placeholder="نام نویسنده مقاله را وارد کنید" />
+            <Form.Control
+              type="text"
+              placeholder="نام نویسنده مقاله را وارد کنید"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
@@ -31,12 +51,13 @@ function AddArticle() {
             <Form.Label>عکس مقاله</Form.Label>
             <Form.Control type="text" placeholder="عکس مقاله را وارد کنید" />
           </Form.Group>
+
           <Form.Group className="mb-3">
-            <Form.Label>مدت زمان خواندن مقاله</Form.Label>
+            <Form.Label>مدت زمان خواندن</Form.Label>
             <Form.Control type="number" placeholder="" />
           </Form.Group>
 
-          <Button variant="primary" type="button">
+          <Button onClick={addArticleHandler} variant="primary" type="button">
             ساخت مقاله
           </Button>
         </Form>
