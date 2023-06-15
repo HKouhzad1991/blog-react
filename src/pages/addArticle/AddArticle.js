7cd26426686ccb23bd86affe02add1cc56332d5d
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-15 16:37:34 +0430

    eventhandler usestate being shorter

--- src/pages/addArticle/AddArticle.js
+++ src/pages/addArticle/AddArticle.js
@@ -1,49 +1,47 @@
 import MyNavbar from "../../components/navbar/MyNavbar";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import './AddArticle.css'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./AddArticle.css";
 import axios from "axios";
 import { useState } from "react";
 
-
 function AddArticle() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [writter, setWritter] = useState("");
+  const [category, setCategory] = useState("");
+  const [image, setImage] = useState("");
+  const [readingTime, setReadingTime] = useState("");
 
-    const [titleState , setTitleState] = useState('')
-    const [descState , setDescState] = useState('')
-    const [writterState , setWritterState] = useState('')
-    const [categoryState , setCategoryState] = useState('')
-    const [imageState , setImagetate] = useState('')
-    const [readingTimeState , setReadingTimeState] = useState('')
+  const addArticleHandler = () => {
+    axios.post("http://localhost:5000/articles", {
+      title,
+      desc,
+      writter,
+      category,
+      image,
+      readingTime,
+    })
+  };
 
-    const addArticleHandler = () => {
-        axios.post('http://localhost:5000/articles' , {
-            title : titleState,
-            desc : descState,
-            writter : writterState,
-            category : categoryState,
-            image : imageState,
-            readingTime : readingTimeState
-        } )
-    }
-
-    const titleInputHanlder = (e) => {
-        setTitleState(e.target.value)
-    }
-    const descInputHanlder = (e) => {
-        setDescState(e.target.value)
-    }
-    const writterInputHanlder = (e) => {
-        setWritterState(e.target.value)
-    }
-    const categoryInputHanlder = (e) => {
-        setCategoryState(e.target.value)
-    }
-    const imageInputHanlder = (e) => {
-        setImagetate(e.target.value)
-    }
-    const readingTimeInputHanlder = (e) => {
-        setReadingTimeState(e.target.value)
-    }
+  const titleInputHanlder = (e) => {
+    setTitle(e.target.value);
+  };
+  const descInputHanlder = (e) => {
+    setDesc(e.target.value);
+  };
+  const writterInputHanlder = (e) => {
+    setWritter(e.target.value);
+  };
+  const categoryInputHanlder = (e) => {
+    setCategory(e.target.value);
+  };
+  const imageInputHanlder = (e) => {
+    setImage(e.target.value);
+  };
+  const readingTimeInputHanlder = (e) => {
+    setReadingTime(e.target.value);
+  };
 
   return (
     <>
@@ -52,34 +50,58 @@ function AddArticle() {
         <Form>
           <Form.Group className="mb-3">
             <Form.Label>عنوان مقاله</Form.Label>
-            <Form.Control onChange={titleInputHanlder} type="text" placeholder="عنوان مقاله را وارد کنید" />
+            <Form.Control
+              onChange={titleInputHanlder}
+              type="text"
+              placeholder="عنوان مقاله را وارد کنید"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>توضیح کوتاه</Form.Label>
-            <Form.Control onChange={descInputHanlder} type="text" placeholder="یه توضیح کوتاه در مورد مقاله وارد کنید" />
+            <Form.Control
+              onChange={descInputHanlder}
+              type="text"
+              placeholder="یه توضیح کوتاه در مورد مقاله وارد کنید"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>نویسنده مقاله</Form.Label>
-            <Form.Control onChange={writterInputHanlder} type="text" placeholder="نام نویسنده مقاله را وارد کنید" />
+            <Form.Control
+              onChange={writterInputHanlder}
+              type="text"
+              placeholder="نام نویسنده مقاله را وارد کنید"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>موضوع مقاله</Form.Label>
-            <Form.Control onChange={categoryInputHanlder} type="text" placeholder="موضوع مقاله را وارد کنید" />
+            <Form.Control
+              onChange={categoryInputHanlder}
+              type="text"
+              placeholder="موضوع مقاله را وارد کنید"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>عکس مقاله</Form.Label>
-            <Form.Control onChange={imageInputHanlder} type="text" placeholder="عکس مقاله را وارد کنید" />
+            <Form.Control
+              onChange={imageInputHanlder}
+              type="text"
+              placeholder="عکس مقاله را وارد کنید"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>مدت زمان خواندن</Form.Label>
-            <Form.Control onChange={readingTimeInputHanlder} type="number" placeholder="" />
+            <Form.Control
+              onChange={readingTimeInputHanlder}
+              type="number"
+              placeholder=""
+            />
           </Form.Group>
-          
+
           <Button onClick={addArticleHandler} variant="primary" type="button">
             ساخت مقاله
           </Button>
