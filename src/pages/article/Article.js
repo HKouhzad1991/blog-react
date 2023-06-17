13b5478e4ef4a797b5f6330faae769d126264468
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-17 16:50:51 +0430

    data in response

--- src/pages/article/Article.js
+++ src/pages/article/Article.js
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Article() {
-    const articleId = useParams().articleId
-    const [articleData, setArticleId] = useState({})
-    
-    useEffect(() => {
-        axios.get("http://localhost:3000/article/${articleId}");
-        .then(response => console.log(response))
-    } , {})
-  
+  const articleId = useParams().articleId;
+  const [articleData, setArticleId] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/articles/${articleId}")
+      .then((response) => console.log(response));
+  }, []);
 
   return <h1>Article Page</h1>;
 }
