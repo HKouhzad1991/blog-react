5d56b41dfc288bec347b68587246d0bc0f94e907
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-18 19:22:27 +0430

    get request with useeffect

--- src/pages/editArticle/EditArticle.js
+++ src/pages/editArticle/EditArticle.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function EditArticle() {
   const [articleData, setArticleData] = useState();
-  const articleId = useParams().articleId;
+    const articleId = useParams().articleId;
+    
+
+    useEffect
 
   return <h1>EditArticle Page</h1>;
 }
