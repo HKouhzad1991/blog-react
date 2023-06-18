7af580daf79d877ad0f4c6ef90835837b3e78043
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-18 19:21:10 +0430

    usestate

--- src/pages/editArticle/EditArticle.js
+++ src/pages/editArticle/EditArticle.js
@@ -1,10 +1,10 @@
+import { useParams } from "react-router-dom";
+
 function EditArticle() {
+  const [articleData, setArticleData] = useParams();
   const articleId = useParams().articleId;
 
-    
-    return (
-        <h1>EditArticle Page</h1>
-    );
+  return <h1>EditArticle Page</h1>;
 }
 
 export default EditArticle;
